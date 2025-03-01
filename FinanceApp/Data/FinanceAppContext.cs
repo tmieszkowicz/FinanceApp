773a0efdd30a0d9bf76e0a0ef99432ea3f135a70
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2025-03-01 17:47:34 +0100

    initial db setup

---
+++ FinanceApp/Data/FinanceAppContext.cs
@@ -0,0 +1,14 @@
+﻿using FinanceApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Data;
+
+public class FinanceAppContext : DbContext
+{
+    public FinanceAppContext(DbContextOptions<FinanceAppContext> options):base(options)
+    {
+        
+    }
+
+    DbSet<Expense> Expenses { get; set; }
+}
--- FinanceApp/Program.cs
+++ FinanceApp/Program.cs
@@ -1,15 +1,19 @@
+using FinanceApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<FinanceAppContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
