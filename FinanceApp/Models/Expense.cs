b92d33ecee22a30bf44363ab8822e779380e2661
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2025-03-01 17:02:15 +0100

    add expense class

---
+++ FinanceApp/Models/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApp.Models;
+
+public class Expense
+{
+    public int Id { get; set; }
+    [Required]
+    public string Description { get; set; } = string.Empty;
+    [Required]
+    [Range (0.01, double.MaxValue, ErrorMessage = "Expense amount needs to be higher than zero.")]
+    public double Amount { get; set; }
+    [Required]
+    public string Category { get; set; } = string.Empty;
+    public DateTime Date { get; set; } = DateTime.Now;
+}
