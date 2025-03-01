d196a69dded97fa09a691e185ef87879c7b32852
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2025-03-01 20:48:06 +0100

    add expense controller and view

---
+++ FinanceApp/Controllers/ExpenseController.cs
@@ -0,0 +1,18 @@
+﻿using FinanceApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceApp.Controllers;
+
+public class ExpenseController : Controller
+{
+    private readonly FinanceAppContext _context;
+    public ExpenseController(FinanceAppContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Index()
+    {
+        var expenses = _context.Expenses.ToList();
+        return View(expenses);
+    }
+}
